01fa5dbbed554203046ba89ee7e658600fed6cc7
vladrph <Vrpetithomme2599@eagle.fgcu.edu>
2019-08-28 19:27:08 -0400

    Added Button

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,10 +6,12 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -47,6 +49,18 @@ public class Main extends Application {
         PasswordField pwBox = new PasswordField();
 
         grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+
         primaryStage.setScene(scene);
 
         primaryStage.show();
