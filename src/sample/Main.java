a1bc12d721bab73d47af70b2b8150d2100a0c4b1
vladrph <Vrpetithomme2599@eagle.fgcu.edu>
2019-08-28 20:27:08 -0400

    Added Button started Css Create an ID for each text node by using the setID() method of the Node class:
    scenetitle.setId("welcome-text");
    
    actiontarget.setId("actiontarget");
    
    Added Example 3-4 Text Effect
    
    Example 3-6 Button Hover Style
    Added Hello World

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("JavaFX ");
         primaryStage.setScene(new Scene(root, 300, 275));
 
         GridPane grid = new GridPane();
